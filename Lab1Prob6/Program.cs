82046b4c990a3899bccd009fb75ad37d5b16cd9d
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-19 21:29:15 +0300

    Prob6

--- Lab1Prob6/Program.cs
+++ Lab1Prob6/Program.cs
@@ -8,6 +8,39 @@ namespace Lab1Prob6
     {
         static void Main(string[] args)
         {
+            /*Ex6
+Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele trei valori in ordine
+descrescatoare.
+• Exemplu: citim 3,9,0 Afisam : 9 3 0  */
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            
+
+            if (x > y && x > z && y > z)
+            { 
+                Console.WriteLine("Numerele sunt puse descrescator " + x + "," + y + "," + z);
+            }
+             else if (x > y && x > z && z > y)
+                {
+                Console.WriteLine("Numerele sunt puse descrescator " + x + "," + z + "," + y);
+                }
+                else if (y > x && y > z && x > z)
+                 {
+                Console.WriteLine("Numerele sunt puse descrescator " + y + "," + x + "," + z);
+                 }
+                     else if (y > x && y > z && z > x)
+                      {
+                      Console.WriteLine("Numerele sunt puse descrescator " + y + "," + z + "," + x);
+                      }
+                      else if (z > x && z > y && y > x)
+                       {
+                       Console.WriteLine("Numerele sunt puse descrescator " + z + "," + y + "," + x);
+                       }
+                       else if (z > x && z > y && x > y)
+                        {
+                        Console.WriteLine("Numerele sunt puse descrescator " + z + "," + x + "," + y);
+                        }
         }
     }
 }
